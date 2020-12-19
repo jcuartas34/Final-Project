d2457341c570eac39ee74970492aec4069998452
Juan Cuartas <jcuartas@uncc.edu>
2020-12-18 23:58:40 -0500

    added build folder

--- Final-Project-main/server.js
+++ Final-Project-main/server.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const cors = require('cors');
+const express = require('express');
+const mysql = require('mysql');
 const fs = require("fs");
 const app = express();
 const port = 3000;
 
 app.use(cors());
+
+var connection = mysql.createConnection({
+    host     : 'sql9.freemysqlhosting.net',
+    user     : 'sql9382969',
+    password : 'xnzgDr4bg8',
+    database : 'sql9382969'
+});
 
 app.use('/', express.static('public'));
 
